d667e7f0ccf2d7ab881bd456a2d67ebcc84e2e08
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2021-09-16 13:39:25 +0900

    HTML&CSS ABOUT :: basic
    js/main.js modify
    
    About page Basic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// Handle Click on "Contact me" button on home
-const homeContactBtn = document.querySelector('.home__button');
-homeContactBtn.addEventListener('click', ()=> {
-    scrollIntoView('#footer')
-});
-
-
-function scrollIntoView(selector){
-    const scrollTo = document.querySelector(selector); //scrollIntoView엘리멘트 의 부모 컨테이너로 스크롤되는 메소드
-    scrollTo.scrollIntoView({behavior: "smooth"});
-}
-
 // Toggle button Mobile Navigation
 
 const mobileNav = document.querySelector('.mobile__nav');
@@ -25,3 +13,15 @@ toggleBtn.addEventListener('click', () =>{
 deleteBtn.addEventListener('click', ()=>{
     mobileNav.classList.remove('visible');
 });
+
+// Handle Click on "Contact me" button on home
+const homeContactBtn = document.querySelector('.home__button');
+homeContactBtn.addEventListener('click', ()=> {
+    scrollIntoView('#footer')
+});
+
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector); //scrollIntoView엘리멘트 의 부모 컨테이너로 스크롤되는 메소드
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
